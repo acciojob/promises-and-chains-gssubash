d62239cb3bc4da8f56fe75cfdea9b415cb386f83
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-23 19:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const forms= document.getElementById('forms');
+
+
+forms.addEventListener('submit',(e)=>{
+	const obj = e.target;
+	
+e.preventDefault();
+	const name = obj.user.value;
+	const age = obj.age.value;
+ 
+	const pro = new Promise((rs,rj)=>{
+		setTimeout(()=>{
+		if(age>18){
+			
+				rs(`Welcome, ${name}. You can vote.`);
+			
+		}else{
+			rj(`Oh sorry ${age}. You aren't old enough.`);
+		}
+					   },4000);
+	});
+
+	checkAge(pro);
+});
+
+
+async function checkAge(data){
+	try{
+		const result = await data;
+	alert(result);
+	}catch(err){
+		alert(err);
+	}
+}
