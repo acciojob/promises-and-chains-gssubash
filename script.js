aa3cd7be113c978c57266b48c139961d6d1fbb70
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-23 20:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
-//your JS code here. If required.
-const forms= document.getElementById('forms');
+const forms = document.getElementById('forms');
 
+forms.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const obj = e.target;
+  const name = obj.name.value;
+  const age = obj.age.value;
 
-forms.addEventListener('submit', async (e)=>{
-	const obj = e.target;
-	
-e.preventDefault();
-	const name = obj.name.value;
-	const age = obj.age.value;
- 
-	const pro = new Promise((rs,rj)=>{
-		setTimeout(()=>{
-		if(age>18){
-			
-				rs(`Welcome, ${name}. You can vote.`);
-			
-		}else{
-			rj(new Error(`Oh sorry ${age}. You aren't old enough.`));
-		}
-					   },4000);
-	});
+  const pro = new Promise((rs, rj) => {
+    setTimeout(() => {
+      if (age > 18) {
+        rs(`Welcome, ${name}. You can vote.`);
+      } else if(age<18){
+        rj(new Error(`Oh sorry ${age}. You aren't old enough.`));
+      }
+    }, 4000);
+  });
 
-	try{
-		const result = await pro;
-	alert(result);
-	}catch(err){
-		alert(err.message);
-		console.log(err.message);
-	}
+  try {
+    const result = await pro;
+    alert(result);
+  } catch (err) {
+    alert(err.message);
+    console.log(err.message);
+  }
 });
-
-
-async function checkAge(data){
-	try{
-		const result = await data;
-	alert(result);
-	}catch(err){
-		alert(err.message);
-		console.log(err.message);
-	}
-}
-
-
